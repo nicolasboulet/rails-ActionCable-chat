9825c9672fc844eb9d48a2f8dcef47fcd711dcd8
Nicolas Boulet <nicolasboulet@live.fr>
2020-04-05 16:08:59 +0200

    generate chatroom controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-chatroom.create(name: general)
-User.create(email: "test@test.com", nickname: "test1", password: "123456")
-User.create(email: "test@test.com", nickname: "test2", password: "123456")
+Chatroom.create!(name: "general")
+User.create!(email: "test@test.com", nickname: "test1", password: "123456")
+User.create!(email: "test1@test.com", nickname: "test2", password: "123456")
